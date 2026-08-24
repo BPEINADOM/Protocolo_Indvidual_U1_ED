bf74bc9cf2a7d698de077601d6ba758c7526d08d
BPEINADOM <bpeinadom@unicartagena.edu.co>
2026-08-24 16:53:08 -0500

    feat(ejemplo2): implementar recorridos con for y for-each

--- matricesarreglos/src/main/java/co/edu/udec/arreglos/EjercicioArreglos.java
+++ matricesarreglos/src/main/java/co/edu/udec/arreglos/EjercicioArreglos.java
@@ -1,6 +1,7 @@
 package co.edu.udec.arreglos;
 
 import java.util.Random;
+
 public class EjercicioArreglos {
     public static void main(String[] args) {
 
@@ -13,6 +14,22 @@ public class EjercicioArreglos {
         // Usamos un for para llenar las posiciones del arreglo
         for (int i = 0; i < numeros.length; i++) {
             numeros[i] = random.nextInt(50);
+        }
+
+        // Aplicamos el recorrido usando for
+
+        // Recorrido usando for clasico
+        System.out.println("Recorrido con for clasico");
+
+        for (int i = 0; i < numeros.length; i++) {
+            System.out.println(numeros[i]);
+        }
+
+        // Recorrido usando for-each
+        System.out.println("\nRecorrido con for each");
+
+        for (int numero : numeros) {
+            System.out.println(numero);
         }
     }
 }
