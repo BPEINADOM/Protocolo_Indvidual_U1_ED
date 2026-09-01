55905be66f1c9350fc24e1d5b74794fcc938edde
BPEINADOM <bpeinadom@unicartagena.edu.co>
2026-09-01 18:19:38 -0500

    feat(matriz): imprimir y recorrer matriz por columnas

--- matricesarreglos/src/main/java/co/edu/udec/matrices/EjercicioMatrices.java
+++ matricesarreglos/src/main/java/co/edu/udec/matrices/EjercicioMatrices.java
@@ -15,5 +15,26 @@ public class EjercicioMatrices {
                 valor++;
             }
         }
+
+        // Imprimimos la matriz
+        System.out.println("Matriz 3x3:");
+
+        // Hacemos con ciclo for para imprimir la matriz
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                System.out.print(matriz[i][j] + "\t");
+            }
+            System.out.println();
+        }
+
+        // Recorrido de la matriz por columnas
+        System.out.println("\nRecorrido por columnas:");
+
+        // Ciclo for para recorrer las columnas y las filas de la matriz
+        for(int j=0; j < matriz.length; j++) {
+            for(int i=0; i < matriz.length; i++) {
+                System.out.println(matriz[i][j]);
+            }
+        }
     }
 }
