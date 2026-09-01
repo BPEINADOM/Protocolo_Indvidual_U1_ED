e7cfac0b516f545a3395e20acf45412df85e33f4
BPEINADOM <bpeinadom@unicartagena.edu.co>
2026-09-01 18:49:31 -0500

    feat(matriz): sumar elementos e intercambiar filas

--- matricesarreglos/src/main/java/co/edu/udec/matrices/EjercicioMatrices.java
+++ matricesarreglos/src/main/java/co/edu/udec/matrices/EjercicioMatrices.java
@@ -36,5 +36,35 @@ public class EjercicioMatrices {
                 System.out.println(matriz[i][j]);
             }
         }
+
+        // Suma de los elementos de la matriz
+        int suma = 0;
+
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                suma += matriz[i][j];
+            }
+        }
+
+        System.out.println("\nSuma de los elementos de la matriz: " + suma);
+
+        // Intercambio de la primera fila por la ultima
+
+        int[] filaTemp = matriz[0];
+
+        matriz[0] = matriz[matriz.length - 1];
+
+        matriz[matriz.length - 1] = filaTemp;
+
+        // Imprimimos la matriz despues del intercambio
+        System.out.println("\nMatriz despues del intercambio de filas:");
+
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                System.out.print(matriz[i][j] + "\t");
+            }
+            System.out.println();
+        }
+
     }
 }
