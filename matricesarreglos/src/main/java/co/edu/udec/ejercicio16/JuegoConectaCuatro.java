781a187ad7f39b8f1d657d4bc8936232b3a6d7a9
BPEINADOM <bpeinadom@unicartagena.edu.co>
2026-09-07 17:53:51 -0500

    feat(conecta-4): mostrar tablero

--- matricesarreglos/src/main/java/co/edu/udec/ejercicio16/JuegoConectaCuatro.java
+++ matricesarreglos/src/main/java/co/edu/udec/ejercicio16/JuegoConectaCuatro.java
@@ -13,5 +13,25 @@ public class JuegoConectaCuatro {
                 tablero[i][j] = ' ';
             }
         }
+
+        // Mostramos el tablero inicial
+        mostrarTablero(tablero);
+
+    }
+
+    public static void mostrarTablero(char[][] tablero) {
+        System.out.println();
+
+        for (int i = 0; i < tablero.length; i++) {
+            System.out.print("|");
+
+            for (int j = 0; j < tablero[i].length; j++) {
+                System.out.print(" " + tablero[i][j] + "|");
+            }
+
+            System.out.println();
+        }
+
+        System.out.println(" 1  2  3  4  5  6  7");
     }
 }
